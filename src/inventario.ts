ebb6430bbfd709fe10dae7dfae9621d2ac54908e
Jose Mederos <alu0101368832@ull.edu.es>
2025-03-16 15:19:17 +0000

    Añadir llamadas a managers

--- src/inventario.ts
+++ src/inventario.ts
@@ -4,6 +4,10 @@ import { IBien, Bien } from "../src/bien.js";
 import { IMercader, Mercader } from "../src/mercader.js";
 import { ICliente, Cliente } from "../src/cliente.js";
 import { ITransaccion, Transaccion } from "../src/transaccion.js";
+import { BienManager } from "./bienManager.js";
+import { MercaderManager } from "./mercaderManager.js";
+import { ClienteManager } from "./clienteManager.js";
+import { TransaccionManager } from "./transaccionManager.js";
 import { DataSchema } from "../src/database.js";
 
 /**
@@ -11,6 +15,10 @@ import { DataSchema } from "../src/database.js";
  */
 export class Inventario {
   private db: LowSync<DataSchema>;
+  private bienManager: BienManager;
+  private mercaderManager: MercaderManager;
+  private clienteManager: ClienteManager;
+  private transaccionManager: TransaccionManager;
 
   constructor() {
     this.db = new LowSync<DataSchema>(new JSONFileSync("test-db.json"));
@@ -22,6 +30,43 @@ export class Inventario {
       transacciones: [],
     };
     this.db.write();
+    // Inicializar los manager para que utilicen la misma db
+    this.bienManager = new BienManager(this.db);
+    this.mercaderManager = new MercaderManager(this.db);
+    this.clienteManager = new ClienteManager(this.db);
+    this.transaccionManager = new TransaccionManager(this.db);
+  }
+
+  /**
+   * Getter del manager de bienes.
+   * @returns manager de bienes.
+   */
+  getBienManager(): BienManager {
+    return this.bienManager;
+  }
+
+  /**
+   * Getter del manager de mercaderes.
+   * @returns manager de mercaderes.
+   */
+  getMercaderManager(): MercaderManager {
+    return this.mercaderManager;
+  }
+
+  /**
+   * Getter del manager de clientes.
+   * @returns manager de clientes.
+   */
+  getClienteManager(): ClienteManager {
+    return this.clienteManager;
+  }
+
+  /**
+   * Getter del manager de transacciones.
+   * @returns manager de transacciones.
+   */
+  getTransaccionManager(): TransaccionManager {
+    return this.transaccionManager;
   }
 
   /**
